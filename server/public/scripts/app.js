b25b229bedd1874bf48d1e2648d012489745115d
jpoehler <jpoehler@comcast.net>
2016-03-08 19:59:16 -0600

    So many updates

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,52 +1,80 @@
-$(document).ready(function(){
-    $.ajax({
-      type: "GET",
-      url: "/data",
-      success: function(data){
+var peopleData = [];
+var globalIndex = 0;
+var elementArray = [];
+
+$(document).ready(function () {
+  init();
+});
+
+function init(){
+  $.ajax({
+    type: 'GET',
+    url: '/data',
+    success: function(data){
+      peopleData = data.kappa;
+      createIndexNodes(peopleData);
+      updateDom();
+      updateHighlight();
+    }
+  });
+}
+
+//this button brings up the previous person
+    $('.buttons').on('click', '.prev', function(){
+        globalIndex--;
+        if(globalIndex < 0){
+          globalIndex = peopleData.length - 1;
+             }
+
+        updateDom();
+        updateHighlight();
+});
+
+//This brings up the next person
+      $('.buttons').on('click', '.next', function(){
+          globalIndex++;
+          if(globalIndex >= peopleData.length-1){
+              globalIndex=0;
+             }
+
+          updateDom();
+          updateHighlight();
+        });
 
 //This pushes data to the DOM
-        var kappa = [];
+  function updateDom(){
+      for (var i = 0; i < peopleData.length; i++){
+        $('.people').empty();
 
-        for (var i = 0; i < data.kappa.length; i++){
-        kappa.push(data.kappa[i]);
+        var person = peopleData[globalIndex];
+
+        $('.people').append("<div></div>");
+        var $el = $('.people').children().last();
+        $el.append('<h5> My name is ' + person.name + '</h5>');
+        $el.append('<p> I am from ' + person.location + '</p>');
+        $el.append('<p> My spirit animal is: ' + person.spirit_animal + '</p>');
+        $el.append('<p> I am awesome!!! This is what my chohort has to say about me... "' + person.shoutout + '" </p>');
+
+  }
+}
+
+//This pulls up indicator buttons
+  function createIndexNodes(array){
+        for(var i = 0; i < array.length; i++){
+          $('.carousel-indicators').append('<div class="index-point"></div>');
+          var $el = $('.carousel-indicators').children().last();
+          $el.data('index', i);
+          elementArray.push($el);
         }
-        var i = 0;
-        $('#carousel').children().remove();
-        $('#carousel').append('<h5> My name is ' + data.kappa[i].name + '</h5>');
-        $('#carousel').append('<p> I am from ' + data.kappa[i].location + '</p>');
-        $('#carousel').append('<p> My spirit animal is: ' + data.kappa[i].spirit_animal + '</p>');
-        $('#carousel').append('<p> I am awesome!!! This is what my chohort has to say about me... "' + data.kappa[i].shoutout + '" </p>');
+      }
 
-     //this button brings the last person up
-         $('.buttons').on('click', '.prev', function(){
-             i--;
-             if(i == -1){
-                 i = data.kappa.length -1;
-             }
-             $('#carousel').children().remove();
-             $('#carousel').append('<h5> My name is ' + data.kappa[i].name + '</h5>');
-             $('#carousel').append('<p> I am from ' + data.kappa[i].location + '</p>');
-             $('#carousel').append('<p> My spirit animal is: ' + data.kappa[i].spirit_animal + '</p>');
-             $('#carousel').append('<p> I am awesome!!! This is what my chohort has to say about me... "' + data.kappa[i].shoutout + '" </p>');
-         });
-
-         //This brings up the next person
-         $('.buttons').on('click', '.next', function(){
-             i++;
-             if(i>data.kappa.length-1){
-                 i=0;
-             }
-             $('#carousel').children().remove();
-             $('#carousel').append('<h5> My name is ' + data.kappa[i].name + '</h5>');
-             $('#carousel').append('<p> I am from ' + data.kappa[i].location + '</p>');
-             $('#carousel').append('<p> My spirit animal is: ' + data.kappa[i].spirit_animal + '</p>');
-             $('#carousel').append('<p> I am awesome!!! This is what my chohort has to say about me... "' + data.kappa[i].shoutout + '" </p>');
-
-         });
-      //This pulls up indicator buttons
-        $('.carousel-indicators').append('.active');
-
-    }
-});
-
-});
+      function updateHighlight(){
+        for(var i = 0; i < elementArray.length; i++){
+          var $el = elementArray[i];
+          if(i == globalIndex){
+            $el.addClass("index-hightlight");
+          } else {
+            $el.removeClass("index-hightlight");
+          }
+        }
+      }
